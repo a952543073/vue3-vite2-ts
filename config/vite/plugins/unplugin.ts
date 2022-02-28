facf51958d4ae640d57b21a52e480908beff2f09
施俊杰 <shijunjie@east.net>
2022-02-28 10:11:41 +0800

    去除  ArcoResolver  ElementPlusResolver 使用

--- config/vite/plugins/unplugin.ts
+++ config/vite/plugins/unplugin.ts
@@ -1,10 +1,17 @@
+/*
+ * @Author: ShiJunJie
+ * @Date: 2022-02-23 15:49:57
+ * @LastEditors: ShiJunJie
+ * @LastEditTime: 2022-02-28 10:11:29
+ * @Descripttion: vue模块自动化按需引入
+ */
 /**
  * ---------------------------------------
  * vue模块自动化按需引入
  * =======================================
  * 支持以下模块自动引用，无需使用 import * from ***
- * NaiveUi、ElementPlus、AntDesign、pinia、vue、vue-i18n、vue-router
- * 当然，你习惯手工引用也是可以的，并且开发界面项目建议使用 NaiveUi 框架，真心不错
+ * NaiveUi(比较适合前台网站)、AntDesign(比较适合后台网站)、pinia、vue、vue-i18n、vue-router
+ * 当然，你习惯手工引用也是可以的，并且开发界面项目建议使用 NaiveUi 框架，真心不错 (当前项目使用的AntDesign)
  * ---------------------------------------
  */
 import { Plugin } from 'vite'
@@ -12,10 +19,10 @@ import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {
-  ArcoResolver,
+  // ArcoResolver,
   NaiveUiResolver,
   AntDesignVueResolver,
-  ElementPlusResolver,
+  // ElementPlusResolver,
   VueUseComponentsResolver,
 } from 'unplugin-vue-components/resolvers'
 
@@ -29,7 +36,7 @@ export function configUnpluginPlugin(envs?: ViteEnv, isBuild?: boolean): Plugin 
       resolvers: [
         AntDesignVueResolver(),
         // ArcoResolver(),
-        // NaiveUiResolver(),
+        NaiveUiResolver(),
         // ElementPlusResolver(),
         VueUseComponentsResolver(),
       ],
@@ -38,7 +45,12 @@ export function configUnpluginPlugin(envs?: ViteEnv, isBuild?: boolean): Plugin 
     AutoImport({
       dts: resolve(process.cwd(), './config/types/auto-imports.d.ts'),
       imports: ['vue', 'pinia', 'vue-i18n', 'vue-router'],
-      resolvers: [NaiveUiResolver(), ElementPlusResolver(), AntDesignVueResolver(), VueUseComponentsResolver()],
+      resolvers: [
+        NaiveUiResolver(),
+        //ElementPlusResolver(),
+        AntDesignVueResolver(),
+        VueUseComponentsResolver(),
+      ],
     }),
   ]
 }
--- src/api/sys.js
+++ src/api/sys.js
@@ -13,11 +13,11 @@ const user_url = '/sys/user'
 
 export const USER = {
   /** 获取用户列表 */
-  getList: (parameter) => https.get(`${user_url}/page`, { parameter }),
+  getList: (data) => https.get(`${user_url}/page`, { data }),
   /** 添加用户信息 */
-  add: (parameter) => https.post(`${user_url}`, { parameter }),
+  add: (data) => https.post(`${user_url}`, { data }),
   /** 修改用户信息 */
-  update: (parameter) => https.put(`${user_url}`, { parameter }),
+  update: (data) => https.put(`${user_url}`, { data }),
   /** 根据ID删除用户信息 */
   delById: (id) => https.delete(`${user_url}/${id}`),
   /** 根据ID获取用户信息 */
@@ -28,13 +28,13 @@ const role_url = '/sys/role'
 /** 角色接口 */
 export const ROLE = {
   /** 获取角色列表 */
-  getList: (parameter) => https.get(`${role_url}/page`, { parameter }),
+  getList: (data) => https.get(`${role_url}/page`, { data }),
   /** 获取所有角色清单 */
   getAll: () => https.get(`${role_url}s`),
   /** 添加角色信息 */
-  add: (parameter) => https.post(`${role_url}`, { parameter }),
+  add: (data) => https.post(`${role_url}`, { data }),
   /** 修改角色信息 */
-  update: (parameter) => https.put(`${role_url}`, { parameter }),
+  update: (data) => https.put(`${role_url}`, { data }),
   /** 根据角色ID删除信息 */
   delById: (id) => https.delete(`${role_url}/${id}`),
   /** 根据角色ID获取相信信息 */
@@ -50,7 +50,7 @@ export const FUN = {
 const sync_url = '/sys/sync'
 export const SYNC = {
   /** 保存同步设置信息 */
-  setData: (parameter) => https.post(`${sync_url}/setup`, { parameter }),
+  setData: (data) => https.post(`${sync_url}/setup`, { data }),
   /** 获取同步配置信息 */
   getData: () => https.get(`${sync_url}/setup`),
   /** 获取用户同步数据信息  */
@@ -61,7 +61,7 @@ export const SYNC = {
   /** 获取员工查询状态 */
   getEmploy: () => https.get(`sys/employ/search/status`),
   /** 保存员工查询状态 */
-  setEmploy: (parameter) => https.post(`sys/employ/search/status`, { parameter }),
+  setEmploy: (data) => https.post(`sys/employ/search/status`, { data }),
 }
 
 const send_url = '/sys/send/email'
@@ -69,7 +69,7 @@ export const SEND = {
   /** 获取发信邮箱配置 */
   getData: () => https.get(send_url),
   /** 保存发信服务器设置 */
-  setData: (parameter) => https.post(send_url, { parameter }),
+  setData: (data) => https.post(send_url, { data }),
 }
 
 const alarm_url = 'sys/alarm/config'
@@ -77,19 +77,19 @@ export const ALARM = {
   /** 获取报警配置信息 */
   getData: () => https.get(alarm_url),
   /** 保存报警配置 */
-  setData: (parameter) => https.post(alarm_url, { parameter }),
+  setData: (data) => https.post(alarm_url, { data }),
 }
 
 const tag_url = '/sys/tag'
 export const TAG = {
   /** 查询标签分页列表 */
-  getDataList: (parameter) => https.get(`${tag_url}/page`, { parameter }),
+  getDataList: (data) => https.get(`${tag_url}/page`, { data }),
   /** 根据主键获取标签信息 */
   getDataById: (id) => https.get(`${tag_url}/${id}`),
   /** 添加标签 */
-  add: (parameter) => https.post(tag_url, { parameter }),
+  add: (data) => https.post(tag_url, { data }),
   /** 修改标签 */
-  update: (parameter) => https.put(tag_url, { parameter }),
+  update: (data) => https.put(tag_url, { data }),
   /** 删除标签 */
   delById: (id) => https.delete(`${tag_url}/${id}`),
   /** 获取所有标签信息 */
@@ -97,14 +97,14 @@ export const TAG = {
 }
 
 /** 修改密码 */
-export const updatePassword = (parameter) => https.patch(`account/password`, { parameter })
+export const updatePassword = (data) => https.patch(`account/password`, { data })
 
 /** 系统级二次验证 */
 export const SYS = {
   /** 二次验证-- 发送邮件 */
   sendEmail: (token) => https.get(`/sys/email/send/${token}`),
   /** 验证邮箱同步设置 */
-  postEmailCode: (parameter) => https.post(`/sys/config/email/verify`, { parameter }),
+  postEmailCode: (data) => https.post(`/sys/config/email/verify`, { data }),
   /** 关于系统信息 */
   getConfig: () => https.get(`system/config`),
 }
@@ -114,5 +114,5 @@ export const VERIFY = {
   /** 获取验证配置 */
   getData: () => https.get(`/sys/verify/config`),
   /** 保存验证配置 */
-  setData: (parameter) => https.post(`/sys/verify/config`, { parameter }),
+  setData: (data) => https.post(`/sys/verify/config`, { data }),
 }
