1dcdd0e05a6b38d499a4f48b9f0fde427c8376df
施俊杰 <shijunjie@east.net>
2022-02-28 15:35:19 +0800

    更新

--- src/views/home/Button.tsx
+++ src/views/home/Button.tsx
@@ -2,13 +2,14 @@
  * @Author: ShiJunJie
  * @Date: 2022-02-28 14:47:07
  * @LastEditors: ShiJunJie
- * @LastEditTime: 2022-02-28 15:15:31
+ * @LastEditTime: 2022-02-28 15:34:04
  * @Descripttion:
  */
 
 const Button = defineComponent({
   setup() {
     const data = ref(1)
+
     const clickIn = () => data.value++
     return () => <div onClick={clickIn}>aa123a + {data.value}</div>
   },
